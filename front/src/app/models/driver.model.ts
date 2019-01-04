66ab39da7697e97bb0d49751eeb7de0bb0d2fb43
Oleg Borodin <onborodin@gmail.com>
2019-01-04 17:42:37 +0200

    work

--- front/src/app/models/driver.model.ts
+++ front/src/app/models/driver.model.ts
@@ -1,6 +1,11 @@
+
+import { Vehicles } from './vehicle.model'
+
 export interface Driver {
     id?: number
     name?: string
+    vehicle_count?: number
+    vehicle?: Vehicles
 }
 
 export type Drivers = Driver[]
